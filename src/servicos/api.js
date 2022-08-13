fa4cad63f753707f4ebc7d545a165ca8d101d895
Pedro Fires <pedrof.fires@gmail.com>
2022-08-12 21:07:09 -0300

    fixed connection from axios

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-   baseURL: "https://lorem-ipsum-bd.herokuapp.com/",
+   baseURL: "https://lorem-ipsum-db.herokuapp.com/",
    timeout: "10000",
    headers: {
       "Content-Type": "application/json",
